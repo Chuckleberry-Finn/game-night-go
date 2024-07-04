00c2d0da5ee627346aca3330ca3125c5c8616e7c
Andy <aeudet@gmail.com>
2024-07-04 01:34:01 -0400

    fixes

--- Contents/mods/game-night-go/media/lua/client/gameNight - implementGo.lua
+++ Contents/mods/game-night-go/media/lua/client/gameNight - implementGo.lua
@@ -10,12 +10,12 @@ local goTilesWhite = {}
 for i=1, 100 do
 	table.insert(goTilesBlack, "Go_Stone_Black")
 end
-deckActionHandler.addDeck("GoTilesBlack", goTilesBlack)
+deckActionHandler.addDeck("GoStonesBlack", goTilesBlack)
 
 for i=1, 100 do
 	table.insert(goTilesWhite, "Go_Stone_White")
 end
-deckActionHandler.addDeck("GoTilesWhite", goTilesWhite)
+deckActionHandler.addDeck("GoStonesWhite", goTilesWhite)
 
 gamePieceAndBoardHandler.registerTypes({"Base.Go_Board_Front"})
 
@@ -23,7 +23,9 @@ gamePieceAndBoardHandler.registerSpecial(
 	"Base.Go_Board_Front",{ category = "GameBoard", textureSize = {740,800}, actions = { lock=true }, 
 	alternateStackRendering = { func="DrawTextureCardFace", depth=9, rgb = {0.78, 0.71, 0.41} },
 	altState="Go_Board_Back", shiftAction = "flipPiece"},
-	"Base.GoTilesBlack",{ alternateStackRendering = { func="DrawTextureRoundFace", depth = 1.3, rgb = {0.29, 0.27, 0.29} } },
-	"Base.GoTilesWhite",{ alternateStackRendering = { func="DrawTextureRoundFace", depth = 1.3, rgb = {0.61, 0.62, 0.61} } }
+
+	"Base.GoStonesBlack",{ alternateStackRendering = { func="DrawTextureRoundFace", depth = 1.3, rgb = {0.29, 0.27, 0.29} }, sideTexture = "Black_Stone_Texture" },
+
+	"Base.GoStonesWhite",{ alternateStackRendering = { func="DrawTextureRoundFace", depth = 1.3, rgb = {0.61, 0.62, 0.61} }, sideTexture = "White_Stone_Texture" }
 	)
 	
