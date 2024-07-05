73fbc1477f672b6874c0c7b6e27fd1b935e6333f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-07-05 14:00:42 -0400

    fixes

--- Contents/mods/game-night-go/media/lua/client/gameNight - implementGo.lua
+++ Contents/mods/game-night-go/media/lua/client/gameNight - implementGo.lua
@@ -1,34 +1,43 @@
 
 ---First require this so that these modules can be called on as needed.
 local applyItemDetails = require "gameNight - applyItemDetails"
-local deckActionHandler = applyItemDetails.deckActionHandler
+--local deckActionHandler = applyItemDetails.deckActionHandler
 local gamePieceAndBoardHandler = applyItemDetails.gamePieceAndBoardHandler
 
---- Card Table
-local goStonesBlack = {}
-local goStonesWhite = {}
 
-for i=1, 100 do
-	table.insert(goStonesBlack, "Go_Stone_Black")
-end
-deckActionHandler.addDeck("GoStonesBlack", goStonesBlack)
+gamePieceAndBoardHandler.registerTypes({"Base.Go_Board","Base.GoStonesBlack","Base.GoStonesWhite"})
 
-for i=1, 100 do
-	table.insert(goStonesWhite, "Go_Stone_White")
-end
-deckActionHandler.addDeck("GoStonesWhite", goStonesWhite)
 
-gamePieceAndBoardHandler.registerTypes({"Base.Go_Board_Front"})
+gamePieceAndBoardHandler.registerSpecial("Base.Go_Board",
+		{
+			category = "GameBoard",
+			textureSize = {740,800},
+			actions = { lock=true, flipPiece=true },
+			alternateStackRendering = { func="DrawTextureCardFace", depth=9, rgb = {0.78, 0.71, 0.41} },
+			altState="Go_Board_Back",
+			shiftAction = "flipPiece"
+		}
+)
 
-gamePieceAndBoardHandler.registerSpecial(
-	"Base.Go_Board_Front",{ category = "GameBoard", textureSize = {740,800}, actions = { lock=true }, 
-	alternateStackRendering = { func="DrawTextureCardFace", depth=9, rgb = {0.78, 0.71, 0.41} },
-	altState="Go_Board_Back", shiftAction = "flipPiece"},
-	)
 
-	gamePieceAndBoardHandler.registerSpecial("Base.GoStonesBlack",{ alternateStackRendering = { func="DrawTextureRoundFace", depth = 1.3, rgb = {0.29, 0.27, 0.29} }, sideTexture = "Black_Stone_Texture" },
+gamePieceAndBoardHandler.registerSpecial("Base.GoStonesBlack",
+		{
+			weight = 0.003,
+			shiftAction = "takeOneOffStack",
+			canStack = 100,
+			noRotate=true,
+		}
+)
 
-	"Base.GoStonesWhite",{ alternateStackRendering = { func="DrawTextureRoundFace", depth = 1.3, rgb = {0.61, 0.62, 0.61} }, sideTexture = "White_Stone_Texture" })
+
+gamePieceAndBoardHandler.registerSpecial("Base.GoStonesWhite",
+		{
+			weight = 0.003,
+			shiftAction = "takeOneOffStack",
+			canStack = 100,
+			noRotate=true,
+		}
+)
 
 	
 	
