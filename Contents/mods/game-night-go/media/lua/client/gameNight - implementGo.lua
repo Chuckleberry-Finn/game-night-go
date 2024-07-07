11f1f67b985973dca8513221c3f5feef56394b66
Andy <aeudet@gmail.com>
2024-07-07 02:42:12 -0400

    changes

--- Contents/mods/game-night-go/media/lua/client/gameNight - implementGo.lua
+++ Contents/mods/game-night-go/media/lua/client/gameNight - implementGo.lua
@@ -13,7 +13,7 @@ gamePieceAndBoardHandler.registerSpecial("Base.GoBoard",
 			category = "GameBoard",
 			textureSize = {740,800},
 			actions = { lock=true, flipPiece=true },
-			alternateStackRendering = { func="DrawTextureCardFace", depth=9, rgb = {0.78, 0.71, 0.41} },
+			alternateStackRendering = { func="DrawTextureCardFace", sideTexture="Board_Side_Texture", depth=11, rgb = {0.55, 0.69, 0.47} },
 			altState="GoBoard_Back",
 			shiftAction = "flipPiece"
 		}
@@ -24,7 +24,7 @@ gamePieceAndBoardHandler.registerSpecial("Base.GoStonesBlack",
 		{
 			weight = 0.003,
 			shiftAction = "takeOneOffStack",
-			canStack = 100,
+			canStack = 180,
 			noRotate=true,
 			alternateStackRendering = {func="DrawTextureRoundFace", sideTexture="Black_Stone_Texture", depth = 2, rgb = {0.2, 0.2, 0.2}},
 		}
@@ -35,7 +35,7 @@ gamePieceAndBoardHandler.registerSpecial("Base.GoStonesWhite",
 		{
 			weight = 0.003,
 			shiftAction = "takeOneOffStack",
-			canStack = 100,
+			canStack = 180,
 			noRotate=true,
 			alternateStackRendering = {func="DrawTextureRoundFace", sideTexture="White_Stone_Texture", depth = 2, rgb = {0.9, 0.9, 0.9}},
 		}
